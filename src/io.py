76414b528b3fa3680abfe3a9b386d22ddcbf5475
ym3141 <yitongma7@gmail.com>
2022-02-15 22:45:45 -0800

    Now also save selections and fig options

--- src/io.py
+++ src/io.py
@@ -9,14 +9,27 @@ class sessionSave():
         baseDir = path.dirname(saveFileDir)
 
         self.smplSaveList = []
+        selectedSmpls = [mainUiWindow.smplListModel.itemFromIndex(idx) for idx in mainUiWindow.sampleListView.selectedIndexes()]
         for idx in range(mainUiWindow.smplListModel.rowCount()):
             plotItem = mainUiWindow.smplListModel.item(idx)
             self.smplSaveList.append(_convert_smplPlotItem(plotItem, baseDir))
+
+            if plotItem in selectedSmpls:
+                self.smplSaveList[-1]['selected'] = True
+            else:
+                self.smplSaveList[-1]['selected'] = False
 
         self.gateSaveList = []
         for idx in range(mainUiWindow.gateListModel.rowCount()):
             gateItem = mainUiWindow.gateListModel.item(idx)
             self.gateSaveList.append(_convert_gateItem(gateItem))
+
+        self.plotOptions = dict()
+
+        self.plotOptions['curAxScale'] = mainUiWindow.curAxScales
+        self.plotOptions['curChnl'] = mainUiWindow.curChnls
+        self.plotOptions['curNormOption'] = mainUiWindow.curNormOption
+        self.plotOptions['curPlotType'] = mainUiWindow.curPlotType
         
         pass
 
