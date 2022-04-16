5d11b161185830d616d998bd04795710c3d46657
Yitong Ma <yitongma7@gmail.com>
2022-04-15 19:17:37 -0700

    Now build a app bundle for mac

--- pyi_mac.sh
+++ pyi_mac.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-pyinstaller --noconfirm --distpath ./EasyFlowQ_release_mac/ --workpath ./_temp/ ./pyi_mac.spec
+pyinstaller --noconfirm --windowed --distpath ./EasyFlowQ_release_mac/ --workpath ./_temp/ ./pyi_mac.spec
+codesign -f -s EasyFlowQ_YMa EasyFlowQ_release_mac/EasyFlowQ_mac.app
