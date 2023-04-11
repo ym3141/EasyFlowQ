24faa4ae88bd2f7eb328b7160dab3604199574a8
Yitong Ma <yitongma7@gmail.com>
2023-04-10 18:16:20 -0700

    Fix attemp of the about window

--- Window_About.py
+++ Window_About.py
@@ -4,11 +4,9 @@ from os import path
 import pandas as pd
 import numpy as np
 
-
 wUi, wBase = uic.loadUiType('./uiDesigns/AboutWindow.ui') # Load the .ui file
 
 class aboutWindow(wUi, wBase):
-
     def __init__(self) -> None:
 
         wBase.__init__(self)
