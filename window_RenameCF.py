6f5be8afbe92f7fd906d3304d930d90c68dafeb0
ym3141 <yitongma7@gmail.com>
2023-04-25 19:45:40 -0700

    Finished support for multiple plate in CFrenaming

--- window_RenameCF.py
+++ window_RenameCF.py
@@ -30,8 +30,17 @@ class renameWindow_CF(wUi, wBase):
             if not (reMatch is None):
                 self.splitNames.append((int(reMatch.group(1)), reMatch.group(3), int(reMatch.group(4))))
 
+        self.renames = None
 
-        # self.loadRenameFile(renamingFileDir)
+        # Determine the max plate number
+        self.maxPlateNumber = max([splitname[0] for splitname in self.splitNames])
+        self.renameTableViews = [self.tableView1]
+
+        if self.maxPlateNumber > 1:
+            for idx in range(self.maxPlateNumber - 1):
+                newTabPage = tabPage(self.tabWidget, tblExample=self.tableView1)
+                self.tabWidget.addTab(newTabPage, 'Plate #{0}'.format(idx + 2))
+                self.renameTableViews.append(newTabPage.rnTableView)
 
         self.renamePB.clicked.connect(self.handle_renameConfirm)
         self.reloadPB.clicked.connect(self.handle_reloadXlsx)
@@ -45,13 +54,12 @@ class renameWindow_CF(wUi, wBase):
 
     def handle_renameConfirm(self):
         renameDict = dict()
-        for name, splitName in zip(self.smplNameList, self.splitNames):
-            if name in renameDict:
-                pass
-            elif (self.renameTableModel._data.loc[splitName[1], splitName[2]]):
-                renameDict[name] = self.renameTableModel._data.loc[splitName[1], splitName[2]]
-            else:
-                pass
+        if self.renames is not None:
+            for name, splitName in zip(self.smplNameList, self.splitNames):
+                if not (name in renameDict):
+                    rename = self.renameTableViews[splitName[0] - 1].model().dfData.loc[splitName[1], splitName[2]]
+                    if rename:
+                        renameDict[name] = rename 
 
         self.renameConfirmed.emit(renameDict)
         self.close()
@@ -64,16 +72,18 @@ class renameWindow_CF(wUi, wBase):
         self.loadRenameFile(openFileDir)
 
     def loadRenameFile(self, renamingFileDir):
-        maxPlateNumber = max([splitname[0] for splitname in self.splitNames])
+        # Load the renaming file
+        self.renames = exel2renameTable(renamingFileDir, self.maxPlateNumber)
 
-        renames = exel2renameTable(renamingFileDir, maxPlateNumber)
-        duplicates = findDups(renames)
-        dupColorMaps = colorByDuplicates(renames, duplicates)
-        smplColorMaps = colorBySmplNames(renames, self.splitNames)
+        duplicates = findDups(self.renames)
+        dupColorMaps = colorByDuplicates(self.renames, duplicates)
+        smplColorMaps = colorBySmplNames(self.renames, self.splitNames)
 
-        self.renameTableModel = pandasTableModel(renames[0], foregroundDF=dupColorMaps[0], backgroundDF=smplColorMaps[0])
-        self.tableView1.setModel(self.renameTableModel)
-        pass
+        for idx in range(self.maxPlateNumber):
+            renameTableModel = pandasTableModel(self.renames[idx], foregroundDF=dupColorMaps[idx], backgroundDF=smplColorMaps[idx])
+            renameTableView = self.renameTableViews[idx]
+            renameTableView.setModel(renameTableModel)
+
 
 def exel2renameTable(renamingFileDir, maxPlatN):
     names = pd.read_excel(renamingFileDir, sheet_name=None, header=None)
@@ -83,7 +93,7 @@ def exel2renameTable(renamingFileDir, maxPlatN):
     plateNumber = 1
     for idx in range(len(names)):
         allNameTable.append(names.popitem()[1].fillna('').astype(str))
-        plateNumber = int(np.max([np.ceil(allNameTable[-1].shape[0]/12), plateNumber]))
+        plateNumber = int(np.max([np.ceil(allNameTable[-1].shape[0]/8), plateNumber]))
 
     allNameTable.reverse()
     # Make sure all the tables are (8xN)x12 shape
@@ -92,7 +102,7 @@ def exel2renameTable(renamingFileDir, maxPlatN):
             if not (colName in nameTable):
                 nameTable[colName] = [''] * nameTable.shape[0]
         
-        for idxName in range(8*plateNumber):
+        for idxName in range(8 * plateNumber):
             if not (idxName in nameTable.index):
                 nameTable.loc[idxName] = [''] * 12
 
@@ -106,7 +116,7 @@ def exel2renameTable(renamingFileDir, maxPlatN):
     # Seperate to plates:
     renamePlates = []
     for idx in range(plateNumber):
-        renamePlate = renames.iloc[idx * 0: idx * 0 + 8, 0: 12]
+        renamePlate = renames.iloc[idx * 8: idx * 8 + 8, 0: 12]
         renamePlate.set_axis(np.arange(1, 13), axis='columns', inplace=True)
         renamePlate.set_axis(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], axis='index', inplace=True)
         renamePlate.loc['Legend', [1,2]] = ['Sample exist', 'Duplicated name']
@@ -114,8 +124,13 @@ def exel2renameTable(renamingFileDir, maxPlatN):
         renamePlate.replace(emptyName, '', inplace=True)
         renamePlates.append(renamePlate.copy())
 
-    # if len(renamePlate) < maxPlatN:
-    #     for idx in maxPlatN - len(renamePlate)
+    # append empty dfs if there are more plates
+    if len(renamePlates) < maxPlatN:
+        emptyNamePlate = pd.DataFrame(columns=np.arange(1, 13), index=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+        emptyNamePlate.loc['Legend', [1,2]] = ['Sample exist', 'Duplicated name']
+        emptyNamePlate.fillna('', inplace=True)
+        for idx in range(maxPlatN - len(renamePlates)):
+            renamePlates.append(emptyNamePlate.copy())
 
     return renamePlates
 
@@ -159,6 +174,25 @@ def colorBySmplNames(renamePlates, splitNames):
             pass
 
     return colorPlates
+
+
+class tabPage(QtWidgets.QWidget):
+    def __init__(self, parent, tblExample=None) -> None:
+        super().__init__(parent, flags=QtCore.Qt.Widget)
+
+        self.setLayout(QtWidgets.QHBoxLayout())
+
+        self.rnTableView = QtWidgets.QTableView()
+        if tblExample is not None:
+            self.rnTableView.setStyleSheet(tblExample.styleSheet())
+            self.rnTableView.setFont(tblExample.font())
+            self.rnTableView.horizontalHeader().setDefaultSectionSize(tblExample.horizontalHeader().defaultSectionSize())
+        self.rnTableView.setWordWrap(True)
+
+        self.layout().addWidget(self.rnTableView)
+
+        # self.objectName = title
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
