2b858fc68716d207dfe52467e019bf6cbc5ade2b
Yitong Ma <yitongma7@gmail.com>
2024-12-31 22:52:05 -0800

    Revert "Add version argument support to PyInstaller spec and update version retrieval"
    This reverts commit f3ffbecb571a2f34f16419e332ad54c591933199.

--- release/pyi_win.ps1
+++ release/pyi_win.ps1
@@ -1,6 +1,2 @@
 # conda activate EasyFlowQ
-$_ver = python ./src/EasyFlowQ/__init__.py
-
-pyinstaller --noconfirm --distpath .\release\EasyFlowQ_release_win\ --workpath .\_temp\ .\release\pyi_universal.spec -- --version $_ver
-Compress-Archive -Path .\release\EasyFlowQ_release_win\EasyFlowQ_Bundle_v$_ver `
-    -DestinationPath .\release\EasyFlowQ_release_win\EasyFlowQ_Win_v$_ver.zip
+pyinstaller --noconfirm --distpath .\release\EasyFlowQ_release_win\ --workpath .\_temp\ .\release\pyi_universal.spec
--- src/EasyFlowQ/__init__.py
+++ src/EasyFlowQ/__init__.py
@@ -1,4 +1,1 @@
 __version__ = "1.6"
-
-if __name__ == "__main__":
-    print(__version__)
