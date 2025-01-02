7fe926d6cc1a927d4644ac8aa41ad420e5794a18
Yitong Ma <yitongma7@gmail.com>
2025-01-02 00:41:56 -0800

    Enhance build script: delete previous build and add wait time before compression

--- release/pyi_win.ps1
+++ release/pyi_win.ps1
@@ -1,6 +1,15 @@
 # conda activate EasyFlowQ
 $_ver = python ./src/EasyFlowQ/__init__.py
 
+# delete the previous build
+Remove-Item -Recurse -Force .\release\EasyFlowQ_release_win\
+
+# build the app
 pyinstaller --noconfirm --distpath .\release\EasyFlowQ_release_win\ --workpath .\_temp\ .\release\pyi_universal.spec -- --version $_ver
-Compress-Archive -Path .\release\EasyFlowQ_release_win\EasyFlowQ_Bundle_v$_ver `
+
+# wait 5 second for the process to finish
+Start-Sleep -Seconds 5
+
+# compress the build
+Compress-Archive -Force -Path .\release\EasyFlowQ_release_win\EasyFlowQ_Bundle_v$_ver `
     -DestinationPath .\release\EasyFlowQ_release_win\EasyFlowQ_Win_v$_ver.zip
