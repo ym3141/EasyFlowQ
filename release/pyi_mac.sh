9e50baeb4ea35d1c033d204dc8e5ac3bb369c7a5
Yitong Ma <yitongma7@gmail.com>
2025-01-02 22:22:50 -0800

    MacOS packaing script now include version number in file name

--- release/pyi_mac.sh
+++ release/pyi_mac.sh
@@ -2,11 +2,15 @@
 
 # delete and add folder
 rm -r release/EasyFlowQ_release_mac/*
+rm -r _temp/pyi_*
 mkdir release/EasyFlowQ_release_mac/dmg
+
+# get the current version number
+_ver=$(python ./src/EasyFlowQ/__init__.py)
 
 # package with pyinstaller and codesign
 # conda activate easyflow_env
-pyinstaller --noconfirm --distpath ./release/EasyFlowQ_release_mac/ --workpath ./_temp/ ./release/pyi_mac.spec
+pyinstaller --noconfirm --distpath ./release/EasyFlowQ_release_mac/ --workpath ./_temp/ ./release/pyi_universal.spec -- --version $_ver
 codesign -f -s EasyFlowQ_YMa release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.app
 
 # Copy the app bundle to the dmg folder.
@@ -16,8 +20,8 @@ cp -r release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.app release/EasyFlowQ_releas
 test -f release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.dmg && rm release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.dmg
 
 create-dmg \
-  --volname EasyFlowQ_MACOS.dmg \
+  --volname EasyFlowQ_MACOS_v$_ver.dmg \
   --icon EasyFlowQ_MACOS.app 120 120 \
   --app-drop-link 360 120 \
-  release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.dmg \
+  release/EasyFlowQ_release_mac/EasyFlowQ_MACOS_v$_ver.dmg \
   release/EasyFlowQ_release_mac/dmg/
