22acbb40e0bfebfac78db69ea255af32fb6154d9
Yitong Ma <yitongma7@gmail.com>
2025-03-16 14:51:49 -0700

    Add tests for loading v1.6b save file and validate UI components

--- test/test_window_Main.py
+++ test/test_window_Main.py
@@ -6,6 +6,7 @@ import pytest
 from src.EasyFlowQ.window_Main import mainUi
 from src.EasyFlowQ.window_Settings import localSettings
 from src.EasyFlowQ.backend.efio import sessionSave
+from src.EasyFlowQ.backend.qtModels import pandasTableModel
 
 
 def test_window_Main(qtbot): 
@@ -30,7 +31,7 @@ def test_window_Main(qtbot):
     mWindow.close()
 
 
-def test_loading_eflq(qtbot):
+def test_loading_eflq1_4(qtbot):
     mWindow = mainUi(localSettings(testMode=True))
     qtbot.addWidget(mWindow)
     mWindow.show()
@@ -51,3 +52,41 @@ def test_loading_eflq(qtbot):
     assert artistN == 15, 'Artists number on plot is {0}, instead'.format(artistN)
 
     mWindow.close()
+
+
+def test_loading_eflq1_6b(qtbot):
+    mWindow = mainUi(localSettings(testMode=True))
+    qtbot.addWidget(mWindow)
+    mWindow.show()
+
+    assert mWindow.isVisible()
+
+    # Test loading the v1.6b save file
+    with pytest.warns(UserWarning, 
+                      match='Creating new gate: Logicle parameters are not provided for logicle scaled axis. Using default parameters'):
+        sessionSave.loadSessionSave(mWindow, './demo_sample/SaveTestSimple_v1.6b.eflq')
+    mWindow.set_saveFlag(False) # Avoid prompting the save dialog
+
+    selectedSmpls = mWindow.smplTreeWidget.selectedItems()
+    assert len(selectedSmpls) == 2
+    assert selectedSmpls[1].text(0) == '01-Well-C2', 'Sample name wrong, in loading the v1.6b save file'
+
+    # Test of the plot widegt
+    artistN = len(mWindow.mpl_canvas.ax.get_children())
+    # print(artistN)
+    assert artistN == 15, 'Artists number on plot is {0}, instead'.format(artistN)
+
+    # Test of the stat window
+    mWindow.actionStats_window.trigger()
+    assert mWindow.statWindow.isVisible()
+
+    statDF = mWindow.statWindow.tableView.model()
+    assert type(statDF) == pandasTableModel, 'Stat window does not have a pandasTableModel'
+    assert statDF.dfData.shape == (2, 9), 'Stat window does not have the correct number of rows and columns'
+    
+    logicle_in_gate = statDF.dfData['% of parent in: \nlogicleGate (selected)']
+    assert list(logicle_in_gate) == ['11.07%', '17.06%'], 'Stat window does not have the correct in gate percentage values'
+
+    mWindow.close()
+
+
